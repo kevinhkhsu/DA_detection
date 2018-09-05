c02611e1bc135fcc861664c20677399c2fbe3364
kevin <kevinhkhsu@hotmail.com>
2018-09-05 14:52:11 -0700

    09042018

--- experiments/scripts/test_adapt_faster_rcnn.sh
+++ experiments/scripts/test_adapt_faster_rcnn.sh
@@ -20,7 +20,7 @@ case ${DATASET} in
     TRAIN_IMDB_T="KITTI_fake"
     TEST_IMDB="cityscapes_val"
     STEPSIZE="[50000]"
-    ITERS=50000
+    ITERS=140000
     ANCHORS="[4,8,16,32]"
     RATIOS="[0.5,1,2]"
     ;;
@@ -29,8 +29,8 @@ case ${DATASET} in
     TRAIN_IMDB_T="KITTI_train"
     TEST_IMDB="KITTI_val"
     STEPSIZE="[50000]"
-    ITERS=30000
-    ANCHORS="[8,16,32]"
+    ITERS=70000
+    ANCHORS="[4,8,16,32]"
     RATIOS="[0.5,1,2]"
     ;;
   *)
@@ -47,7 +47,7 @@ set +x
 if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
   NET_FINAL=output/${NET}/${TRAIN_IMDB_S}/${EXTRA_ARGS_SLUG}/${NET}_faster_rcnn_iter_${ITERS}.pth
 else
-  NET_FINAL=output/${NET}/${TRAIN_IMDB_S}/_adapt/${NET}_faster_rcnn_img_imnet_iter_${ITERS}.pth
+  NET_FINAL=output/${NET}/${TRAIN_IMDB_S}/_adapt/${NET}_faster_rcnn_img_K2C_490k_imnet_iter_${ITERS}.pth
 fi
 
 if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
